48bfaa5bb830595ccf41a45bdecacb3dbbb950bc
test <test>
2023-06-28 15:56:04 +0700

    test

---
+++ bank.py
@@ -0,0 +1,9 @@
+def main():
+    s = input ('Приветствие: ' )
+    if ("Здравствуйте") in s:
+        print ("$0")
+    elif s ==("З"):
+        print ("$20")
+    else:
+        print ("$100")
+main()
